478465a30004d955cf6263d90e421a60f95269c6
Guilherme Cunha <guicunhasou@gmail.com>
2025-09-26 01:43:33 -0300

    carrossel para depoimentos

--- js/script.js
+++ js/script.js
@@ -14,3 +14,25 @@ hamburgerBtn.addEventListener("click", () => {
         hamburgerIcon.classList.add("fa-bars");
     }
 });
+
+const track = document.querySelector(".testimonial-track");
+const slides = Array.from(track.children);
+const prevBtn = document.querySelector(".carousel-btn.prev");
+const nextBtn = document.querySelector(".carousel-btn.next");
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
